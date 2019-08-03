513a3fc04dadac0be967492f97b9b3e899481379
zachlas <zacharylasiuk@gmail.com>
2019-08-02 22:54:18 -0400

    nodejs app dev

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,18 @@
-# docker run -v ./app:/usr/src/app 
-
+# Reference:
+# https://medium.com/@deanslamajr/develop-that-nodejs-app-inside-a-docker-container-d7b93b8c89db
 
 FROM node:12
 
-# Create app directory and
-#   set dir for Dockerfile commands
-#WORKDIR /usr/src/app
+# Install dependencies
+WORKDIR /usr/src/app
+COPY ./app/package.json ./app/package-lock.json* ./
+RUN npm cache clean --force && npm install
 
-# Open port
+# copy app source to image _after_ npm install so that
+# application code changes don't bust the docker cache of npm install step
+COPY . /opt/app
+
+# Expose docker PORT 8080
 EXPOSE 8080
 
 # Start my server
--- app/index.js
+++ app/index.js
@@ -1,25 +1,31 @@
 var app = require('express')();
 var http = require('http').Server(app);
-
 var io = require('socket.io')(http);
 
 app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
+ 	res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', function(socket){
-  console.log('a user connected');
+	// Basic functionality
+	console.log('a user connected');
+	socket.on('disconnect', function(){
+		console.log('user disconnected');
+	});
 
-  socket.on('custom-event', function(value){
-    io.emit('custom-event', value);
-  });
 
-  socket.on('disconnect', function(){
-    console.log('user disconnected');
-  });
+
+
+	socket.on('custom-event', function(value){
+		io.emit('custom-event', value);
+	});
+
+
+
+
 });
 
 
 http.listen(8080, function(){
-  console.log('listening on *:8080');
+ 	console.log('listening on *:8080');
 });
